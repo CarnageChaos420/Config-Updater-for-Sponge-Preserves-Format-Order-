692f7f53ff11c64c36166755e470a9ef448fa714
CarnageChaos420 <53155558+CarnageChaos420@users.noreply.github.com>
2021-07-06 03:56:48 -0500

    Update MyConfigNode.java

--- MyConfigNode.java
+++ MyConfigNode.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 
@@ -21,7 +20,7 @@ public class MyConfigNode {
 	public List<String> getLines(){ return this.lines; }
 	
 	public void addChild(String key, MyConfigNode node){ this.children.put(key, node); }
-	public HashMap<String, MyConfigNode> getChildren() { return this.children; }
+	public LinkedHashMap<String, MyConfigNode> getChildren() { return this.children; }
 	
 	/**
 	 * Merges the two nodes together. Preserves the order/comments/new values of the calling node, while overwriting any
@@ -30,7 +29,7 @@ public class MyConfigNode {
 	 */
 	public void merge(MyConfigNode node){
 		for(MyConfigNode child : node.getChildren().values()){
-			children.put(child.getKey(), child);
+			this.children.put(child.getKey(), child);
 		}
 	}
 	
@@ -40,7 +39,7 @@ public class MyConfigNode {
 	 */
 	public List<String> toStringList(){
 		List<String> list = new ArrayList<String>();
-		for(MyConfigNode child : children.values()){
+		for(MyConfigNode child : this.children.values()){
 			list.addAll(child.getLines());
 		}
 		return list;
